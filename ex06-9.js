ba49be356ea65c26e730f982cc093c72c0aec72e
sanjibiglove <sanjibiglove@gmail.com>
2018-12-02 20:07:23 -0500

    1203-1

--- ex06-9.js
+++ ex06-9.js
@@ -1,2 +1,2 @@
 function txtchange1(){document.getElementByld('txt1').innerHTML = 'ナンバーワン!';}
-function imgchange1(){document.getElementById('logo').src ='.medamayaki.jpg';}
+function imgchange1(){document.getElementById('logo').src ='medamayaki.jpg';}
