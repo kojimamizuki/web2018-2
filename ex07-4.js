9cc32ed7bac1238586092f3a5e17165eaa612ec5
sanjibiglove <sanjibiglove@gmail.com>
2018-12-16 20:53:03 -0500

    1217

--- ex07-4.js
+++ ex07-4.js
@@ -1,2 +1,2 @@
-function add(){var x = document.getElementById ('input3').value;
-               var y = document.getElementById ('input4').value;alert(Number(x)÷(Number(y)×Number(y)));}
+function add(){var u = document.getElementById ('input3').value;
+               var z = document.getElementById ('input4').value;alert(Number(u)/(Number(z)*Number(z)));}
