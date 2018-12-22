e3814a54b8a8218a995c6f36793168889a8cacb1
sanjibiglove <sanjibiglove@gmail.com>
2018-12-22 14:53:02 -0500

    1223-1

--- ex07-3.js
+++ ex07-3.js
@@ -1,7 +1,6 @@
 
 function add(){var x = document.getElementById ('input1').value;
-               var y = document.getElementById ('input2').value;
-                       alert(Number(x)+Number(y));}
+               var y = document.getElementById ('input2').value;alert(Number(x)+Number(y));}
 
 
 
